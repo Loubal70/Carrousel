962229c9df4fcb209ce1eeab7521e1fb46e824c5
Loubal70 <63310494+Loubal70@users.noreply.github.com>
2021-04-28 16:42:05 +0200

    v1.0.0

--- index.php
+++ index.php
@@ -3,55 +3,71 @@
   <head>
     <meta charset="utf-8">
     <title>FS Carrousel</title>
+
     <!-- CSS Slick -->
-    <link rel="stylesheet" type="text/css" href="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.css"/>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick-theme.min.css">
+    <!-- Make Hamburger -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/hamburgers/0.8.1/hamburgers.min.css">
+
+    <!-- Import Icon -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.1/css/all.css">
 
     <!-- Own CSS -->
+    <link rel="stylesheet" href="src/css/normalize.css">
     <link rel="stylesheet" href="src/css/main.css">
 
   </head>
   <body>
+
+    <!-- Hamburger Menu -->
+
+    <button id="simpleHamburger" class="hamburger hamburger--spring" onclick="this.classList.toggle('is-active')" type="button">
+      <span class="hamburger-box">
+        <span class="hamburger-inner"></span>
+      </span>
+    </button>
+
+    <!-- Menu -->
+
+     <nav>
+       <span>Menu</span>
+       <ul class="languages">
+         <li>EN</li>
+         <li>FR</li>
+       </ul>
+
+       <ul class="social-networks">
+         <li><i class="fab fa-facebook-square"></i></li>
+         <li><i class="fab fa-twitter-square"></i></li>
+         <li><i class="fab fa-pinterest-square"></i></li>
+       </ul>
+
+     </nav>
+
+
     <?php
       // Initialisation data
-        require_once('./src/init.php');
-      // Initilisation display variable
-        $html;
-
-      if (!empty($datas)) {
-        foreach ($datas as $data) {
-          $html .= '<div class="carrousel"><div class="">';
-
-            // Image
-              $html .= '<img src="'. $data['ImageUrl'] .'" alt="'. $data['MentionText'] .'">';
-            // Mentions
-              $html .= '<span>'. $data['MentionText'] .'</span>';
-
-          $html .= '</div></div>';
-        }
-      }
-
-
-
+      require_once('./src/init.php');
     ?>
 
+
   </body>
+
+  <!-- Init JQuery -->
+  <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
   <!-- Slick JS -->
-  <script type="text/javascript" src="//cdn.jsdelivr.net/npm/slick-carousel@1.8.1/slick/slick.min.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/slick-carousel/1.8.1/slick.min.js"></script>
+
 
   <script type="text/javascript">
-     // Init JQuery
-     const { JSDOM } = require( "jsdom" );
-     const { window } = new JSDOM( "" );
-     const $ = require( "jquery" )( window );
 
-     $(document).ready(function(){
-        $('.carousel').slick({
-        slidesToShow: 3,
-        dots:true,
-        centerMode: true,
-        });
-      });
-
+   $(document).ready(() => {
+     $('#carrousel').slick({
+       autoplay: false,
+       speed: 800,
+     });
+   });
 
   </script>
 
--- src/init.php
+++ src/init.php
@@ -2,8 +2,31 @@
 
 $datas = [
   [
-    "ImageUrl"         => "https://image.freepik.com/photos-gratuite/portrait-jeune-belle-femme-gesticulant_273609-40179.jpg",
-    "MentionUrl"  => "mminetwork.fr",
+    "ImageUrl"         => "https://fidex.com.ua/monster/kanter/img/bgn7.jpg",
+    "MentionUrl"  => "https://mminetwork.fr",
     "MentionText" => "Découvrez un nouvel horizon",
+  ],
+  [
+    "ImageUrl"         => "https://wallpaperaccess.com/full/1657858.jpg",
+    "MentionUrl"  => "https://github.com/Loubal70/Carrousel",
+    "MentionText" => "Découvrez un beau profil",
   ]
-]
+];
+
+// Initilisation display variable
+  $html = "";
+
+if (!empty($datas)) {
+  $html .= '<div id="carrousel">';
+  foreach ($datas as $data) {
+    $html .= '<div>';
+      // Image
+        $html .= '<img src="'. $data['ImageUrl'] .'" alt="'. $data['MentionText'] .'">';
+      // Mentions
+        $html .= '<span>'. $data['MentionText'] .'</span>';
+
+    $html .= '</div>';
+  }
+  $html .= '</div>';
+  echo $html;
+}
